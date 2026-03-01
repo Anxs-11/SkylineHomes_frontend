365bef0067d04f4135c92858a3dc3c88123c1372
Mohd Anas <mohd.anas@teradata.com>
2026-03-01 17:30:15 +0530

    updated

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -28,8 +28,6 @@ export const Login: React.FC = () => {
 
       try {
         window.google.accounts.id.initialize({
-          
-          ux_mode:"popup",
           client_id: process.env.REACT_APP_GOOGLE_CLEINT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
@@ -42,6 +40,7 @@ export const Login: React.FC = () => {
           size: "medium",
           type: "standard",
         });
+        window.google.accounts.id.prompt();
       } catch (error) {
         console.log(error);
       }
